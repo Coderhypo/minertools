a203bc499c9ea66edbc630d61b9b8e59d828195c
Coderhypo <i@ihypo.net>
2016-03-21 20:37:35 +0800

    update

---
+++ src/main/java/net/ihypo/block/TitaniumOre.java
@@ -0,0 +1,12 @@
+package net.ihypo.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class TitaniumOre extends Block{
+
+	public TitaniumOre(Material material) {
+		super(material);
+	}
+	
+}
---
+++ src/main/java/net/ihypo/item/TitaniumIngot.java
@@ -0,0 +1,7 @@
+package net.ihypo.item;
+
+import net.minecraft.item.Item;
+
+public class TitaniumIngot extends Item {
+	
+}
